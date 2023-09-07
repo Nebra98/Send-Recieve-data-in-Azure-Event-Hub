7c51c31d1947ddab15ae153df4434a12db473ec0
Nebra98 <bulicbrane561@gmail.com>
2023-09-07 08:52:59 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+
+EVENT_HUB_FULLY_QUALIFIED_NAMESPACE = "your_namespace.servicebus.windows.net"
+EVENT_HUB_NAME = "your_eventhub_name"
+SAS_POLICY_NAME = "RootManageSharedAccessKey"
+SAS_KEY = "your_primary_key"
